debc52dd3f2b65a3405a8be2526a8da64496d576
@thiagofcasa <@thiagofcasa>
2023-07-12 00:04:38 +0100

    Add project files.

---
+++ SevenDaysOfCodeChallenge.API_Access/PokemonApiInvoke.cs
@@ -0,0 +1,50 @@
+﻿using RestSharp;
+using SevenDaysOfCodeChallenge.API_Access.Model;
+using System.Net;
+using System.Text.Json;
+
+namespace SevenDaysOfCodeChallenge.API_Access
+{
+    public class PokemonApiInvoke
+    {
+        public PokeDex GetAllPokemon(string apiUrl)
+        {
+            try
+            {
+                var client = new RestClient(apiUrl);
+                RestRequest request = new RestRequest("", Method.Get);
+
+                var response = client.Execute(request);
+                var result = JsonSerializer.Deserialize<PokeDex>(response.Content);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Mascote GetPokemon(string url)
+        {
+            try
+            {
+                /*var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{id}")*/
+                ;
+                var client = new RestClient(url);
+                RestRequest request = new RestRequest("", Method.Get);
+
+                var response = client.Execute(request);
+
+                var result = JsonSerializer.Deserialize<Mascote>(response.Content);
+
+                return result;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ SevenDaysOfCodeChallenge.UI/Program.cs
@@ -0,0 +1,71 @@
+﻿using SevenDaysOfCodeChallenge.API_Access;
+using SevenDaysOfCodeChallenge.API_Access.Model;
+
+namespace SevenDaysOfCodeChallenge.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            PokemonApiInvoke pkApi = new PokemonApiInvoke();
+            PokeDex pokeDex = new PokeDex();
+
+            pokeDex = pkApi.GetAllPokemon("https://pokeapi.co/api/v2/pokemon/");
+            int sair = 0;
+            Results escolhido = new Results();
+
+            do
+            {
+                Console.WriteLine("Escolha o seu mascote!");
+
+                int qtdMascotes = pokeDex.results.Count();
+
+                Results[] arrayMascotes = new Results[qtdMascotes];
+
+                foreach (var mascote in pokeDex.results)
+                {
+                    Console.WriteLine("Nome: " + mascote.name);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Escreva o nome do mascote escolhido ou tecle 9 para ir para a proxima lista.");
+
+                string nome = Console.ReadLine();
+
+                if (nome == "9")
+                {
+                    pokeDex = pkApi.GetAllPokemon(pokeDex.next);
+                }
+                else
+                {
+                    escolhido = pokeDex.results.Where(m => m.name == nome).FirstOrDefault();
+
+                    if (escolhido != null)
+                    {
+                        //retorna informações do escolhido
+                        sair = 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("O nome do mascote digitado não foi encontrado.");
+                    }
+                }
+
+
+            } while (sair != 1);
+
+            Mascote meuMascote = pkApi.GetPokemon(escolhido.url);
+
+            Console.WriteLine();
+            Console.WriteLine("Nome: " + meuMascote.name);
+            Console.WriteLine("Altura: " + meuMascote.height);
+            Console.WriteLine("Peso:" + meuMascote.weight);
+            foreach (var abilitie in meuMascote.abilities)
+            {
+                Console.WriteLine("Abilidade: " + abilitie.ability.name);
+            }
+        }
+    }
+}
